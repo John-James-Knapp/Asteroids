880c77d8308cb941f774ed341f164bb484307242
John-James-Knapp <john.james.knapp2@gmail.com>
2024-10-12 11:19:46 -0400

    created the player class, currently it just draws the player on the screen. Also updated main() to include drawing the player in the game loop.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import pygame   # type: ignore
+import pygame    # type: ignore
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2) # spawn the player in the middle of the screen.
     dt = 0
 
     while True:
@@ -13,9 +15,10 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
+        dt = clock.tick(60) / 1000 # Limit frames to 60 FPS
 
 
 if __name__ == "__main__":
